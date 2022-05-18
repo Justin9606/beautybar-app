9e33bc0570f4187eee1d0cd3e40eed221c162747
Justin9606 <tonybek.96@gmail.com>
2022-05-18 21:45:42 +0900

    remove duplicated imagepicker code

--- src/screens/community/creatediscussion/CreateDiscussion.js
+++ src/screens/community/creatediscussion/CreateDiscussion.js
@@ -1,13 +1,19 @@
 //react
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 //react native
-import { Dimensions, Image, View, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
+import {
+  Dimensions,
+  Image,
+  View,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
 //useNavigation
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 
 //image picker
 import ImagePicker from 'react-native-image-crop-picker';
-
 
 //styled components
 import styled from 'styled-components';
@@ -35,65 +41,51 @@ import Smalltext from '../../../components/common/Text/SmallText';
 import CreateDiscussionInput from '../components/CreateDiscussionInput';
 import Label from '../components/Label';
 
-import { postData } from '../../../components/svg_data/skin_data'
+import {postData} from '../../../components/svg_data/skin_data';
 
-
-const Creatediscussion = (props) => {
-
-
+const Creatediscussion = props => {
   const navigation = useNavigation();
 
   const [Images, setImages] = useState([]);
   const [tagitem, settagitem] = useState([]);
-  const [tagextaitem, settagextaitem] = useState([])
-  const [discussion, setdiscussion] = useState()
+  const [tagextaitem, settagextaitem] = useState([]);
+  const [discussion, setdiscussion] = useState();
   const [refresh, setrefresh] = useState(false);
   const [loading, setloading] = useState(false);
   const setUpdate = props?.route?.params;
 
-
-
   const imagepicker = () => {
-    setloading(true)
+    setloading(true);
     ImagePicker.openPicker({
       width: 300,
-      height: 400, multiple: true
-    }).then(image => {
-      setImages(image);
-      setloading(false)
-    }).catch(() => {
-      setloading(false)
+      height: 400,
+      multiple: true,
     })
-  }
+      .then(image => {
+        setImages([...Images, ...image]);
+        setloading(false);
+      })
+      .catch(() => {
+        setloading(false);
+      });
+  };
 
-  const imagepicker2 = () => {
-    setloading(true)
-    ImagePicker.openPicker({
-      width: 300,
-      height: 400, multiple: true
-    }).then(image => {
-      setImages([...Images, ...image]);
-      setloading(false)
-    }).catch(() => {
-      setloading(false)
-    })
-  }
+  console.log('images', Images);
 
-  const removeitem = (item) => {
+  const removeitem = item => {
     const indexOfObject = Images.findIndex(object => {
       return object.path === item.path;
     });
     Images.splice(indexOfObject, 1);
     setImages(Images);
     setrefresh(!refresh);
-  }
+  };
 
   const AddDiscussion = () => {
-
-    let ImagesArray = []
+    let ImagesArray = [];
     Images.map((v, index) => {
-      ImagesArray.push({ url: v.path })
-    })
+      ImagesArray.push({url: v.path});
+    });
 
     let data = {
       user_pic: require('../../../assets/icons/temp/user_profile_pic.png'),
@@ -103,16 +95,14 @@ const Creatediscussion = (props) => {
       postImg: ImagesArray,
       like: 20,
       comment: 20,
-      createrdatetime: new Date()
-    }
+      createrdatetime: new Date(),
+    };
 
     postData.push(data);
-    setrefresh(!refresh)
-    setUpdate.setUpdate(data)
-    navigation.goBack()
-
-  }
-
+    setrefresh(!refresh);
+    setUpdate.setUpdate(data);
+    navigation.goBack();
+  };
 
   const Tag1 = tagitem.filter(tagitem => tagitem.isselect === true);
   const Tag2 = tagextaitem.filter(tagextaitem => tagextaitem.isselect === true);
@@ -135,27 +125,36 @@ const Creatediscussion = (props) => {
             <CreateDiscussionInput
               placeholder={'What is on your mind?'}
               multiline={true}
-              onChangeText={(e) => { setdiscussion(e) }}
+              onChangeText={e => {
+                setdiscussion(e);
+              }}
             />
           </Row>
           <Spacer height={24} />
           <TagProductWrap
             activeOpacity={0.7}
-            onPress={() => navigation.navigate('TagProducts', { setTagProduct1: settagitem, setTagProduct2: settagextaitem })}>
+            onPress={() =>
+              navigation.navigate('TagProducts', {
+                setTagProduct1: settagitem,
+                setTagProduct2: settagextaitem,
+              })
+            }>
             <Row justifyContent={'flex-start'}>
               <TagProductSvg />
               <TagProductText>Tag Product</TagProductText>
-              <CountTaggedProduct> {TagProduct.length} Product </CountTaggedProduct>
-              <RightArrowSvg style={{ position: 'absolute', right: 10 }} />
+              <CountTaggedProduct>
+                {' '}
+                {TagProduct.length} Product{' '}
+              </CountTaggedProduct>
+              <RightArrowSvg style={{position: 'absolute', right: 10}} />
             </Row>
           </TagProductWrap>
           <Label label={'Insert Image'} />
 
-
-          {Images.length === 0 ?
+          {Images.length === 0 ? (
             <>
-              {loading === false ?
-                <UploadImgBigWrap activeOpacity={0.7} onPress={() => imagepicker()}>
+              {loading === false ? (
+                <UploadImgBigWrap activeOpacity={0.7} onPress={imagepicker}>
                   <UploadImg />
                   <Spacer height={9.18} />
                   <Smalltext
@@ -164,50 +163,77 @@ const Creatediscussion = (props) => {
                     fontSize={12}
                   />
                 </UploadImgBigWrap>
-                :
+              ) : (
                 <View>
                   <ActivityIndicator size="large" color="#e74779" />
                 </View>
-              }
+              )}
             </>
-            : Images.length === 1 ?
-              <View>
-                <Image source={{ uri: Images[0].path }} style={{ height: 250, width: '100%', alignSelf: 'center', borderRadius: 10 }} />
-                <TouchableOpacity onPress={() => setImages([])} style={{
+          ) : Images.length === 1 ? (
+            <View>
+              <Image
+                source={{uri: Images[0].path}}
+                style={{
+                  height: 250,
+                  width: '100%',
+                  alignSelf: 'center',
+                  borderRadius: 10,
+                }}
+              />
+              <TouchableOpacity
+                onPress={() => setImages([])}
+                style={{
                   position: 'absolute',
                   right: -5,
                   top: -10,
                   backgroundColor: 'black',
-                  height: 30, width: 30, borderRadius: 20
-                }} />
-              </View>
-              : <FlatList
-                extraData={refresh}
-                showsHorizontalScrollIndicator={false}
-                numColumns={2}
-                columnWrapperStyle={{
-                  justifyContent: 'space-between',
-                  padding: 10,
-                  marginBottom: 20,
+                  height: 30,
+                  width: 30,
+                  borderRadius: 20,
                 }}
-                data={Images}
-                renderItem={({ item, index }) => {
-                  return (
-                    <View key={index}>
-                      <Image source={{ uri: item.path }} style={{ height: 150, width: 150, alignSelf: 'center', borderRadius: 10 }} />
-                      <TouchableOpacity onPress={() => removeitem(item)} style={{
+              />
+            </View>
+          ) : (
+            <FlatList
+              extraData={refresh}
+              showsHorizontalScrollIndicator={false}
+              numColumns={2}
+              columnWrapperStyle={{
+                justifyContent: 'space-between',
+                padding: 10,
+                marginBottom: 20,
+              }}
+              data={Images}
+              renderItem={({item, index}) => {
+                return (
+                  <View key={index}>
+                    <Image
+                      source={{uri: item.path}}
+                      style={{
+                        height: 150,
+                        width: 150,
+                        alignSelf: 'center',
+                        borderRadius: 10,
+                      }}
+                    />
+                    <TouchableOpacity
+                      onPress={() => removeitem(item)}
+                      style={{
                         position: 'absolute',
                         right: -5,
                         top: -10,
                         backgroundColor: 'black',
-                        height: 30, width: 30, borderRadius: 20
-                      }} />
-                    </View>
-                  );
-                }}
-              />
-          }
-          <Button title={'Add More Images'} onPress={() => imagepicker2()} />
+                        height: 30,
+                        width: 30,
+                        borderRadius: 20,
+                      }}
+                    />
+                  </View>
+                );
+              }}
+            />
+          )}
+          <Button title={'Add More Images'} onPress={imagepicker} />
         </ScrollableView>
       </>
       <BtnWrap>
